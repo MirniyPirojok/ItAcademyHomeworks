9917a3b488b10e62d2ff39cd53f6f505500295e9
Yaroslav Borisov <MirniyPirojok@gmail.com>
2020-05-29 12:48:48 +0300

    Homework4. 3) Add equals and hashCode 4) Add two interfaces

---
+++ src/Homework4/domain/TechnologyGrade.java
@@ -0,0 +1,6 @@
+package Homework4.domain;
+
+public interface TechnologyGrade {
+    int upgrade(int i);
+    int downgrade(int i);
+}
---
+++ src/Homework4/domain/TransportTrading.java
@@ -0,0 +1,6 @@
+package Homework4.domain;
+
+public interface TransportTrading {
+    int raisePrice(int i);
+    int reducePrice(int i);
+}
